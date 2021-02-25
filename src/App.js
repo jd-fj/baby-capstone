f23073e477fe59c952c2f17f5c757daa3f949f75
taylulz <taylulzcode@gmail.com>
2021-02-25 13:03:12 -0800

    instantiate firebase database through .env keys

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase/app'; //import firebase sdk
 import 'firebase/firestore'; //for authentication
 import 'firebase/auth'; //for authentication
+// import config from './firebase.js'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 //call initializeApp to identify our project 
 firebase.initializeApp({
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
-
+// const { auth } = config.auth();
+// const { firestore } = config.firestore();
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header>
+        <h1>hello</h1>
       </header>
+
+      <section>
+        {user ? <ChatRoom/> : <SignIn/> }
+      </section>
     </div>
   );
+}
+
+function SignIn()  {
+  
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Googs</button>
+  )
+}
+
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Let Me Out</button>
+  )
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-}
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// }
 
-firebase.initializeApp(firebaseConfig);
-firebase.firestore(); 
-// creates and initializes an instance of our Firebase application. pass firebaseConfig as an argument so Firebase knows exactly which Firebase project should be accessed.
+// firebase.initializeApp(firebaseConfig);
+// firebase.firestore(); 
+// // creates and initializes an instance of our Firebase application. pass firebaseConfig as an argument so Firebase knows exactly which Firebase project should be accessed.
+
+// export default firebase;
