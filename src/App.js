6728599139a9c92d9cc217e4c04e7c0f9c347982
taylulz <taylulzcode@gmail.com>
2021-02-25 11:26:59 -0800

    add Firebase and Firestore to application

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app'; //import firebase sdk
+import 'firebase/firestore'; //for authentication
+import 'firebase/auth'; //for authentication
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+//call initializeApp to identify our project 
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
