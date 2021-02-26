c71236f8540a072e6c2c7c71f49715012021a940
taylulz <taylulzcode@gmail.com>
2021-02-26 09:21:32 -0800

    move api key and data out of App into .env

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { useRef, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import firebase from 'firebase/app'; //import firebase sdk
+// import firebase from 'firebase/app'; //import firebase sdk
 import 'firebase/firestore'; //for authentication
 import 'firebase/auth'; //for authentication
 // import config from './firebase.js'
+import firebase from './firebase';
+
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDrD-iQ56W7ESAaNoqEhi8jCf8PwyEkPMk",
-//   authDomain: "chat-tutorial-69c44.firebaseapp.com",
-//   projectId: "chat-tutorial-69c44",
-//   storageBucket: "chat-tutorial-69c44.appspot.com",
-//   messagingSenderId: "471462513622",
-//   appId: "1:471462513622:web:cfa6c9ff677b99ffde3277"
-// }
-// firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
 import firebase from './firebase';
@@ -20,11 +20,11 @@ const rrfProps = {
 }
 
 ReactDOM.render(
-  <Provider>
+  // <Provider>
     <ReactReduxFirebaseProvider {...rrfProps}>
       <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
+    </ReactReduxFirebaseProvider>,
+  // </Provider>,
   document.getElementById('root')
 );
 
